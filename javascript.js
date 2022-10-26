11cf0bb79fb0339e342782682c2bd6a88325104e
MinhDuc <ogdp.vn@gmail.com>
2022-10-26 18:06:53 +0700

    update localtion

--- javascript.js
+++ javascript.js
@@ -23,3 +23,14 @@ function reset__words(){
     words__contents.style.fontFamily = "Poppins, sans-serif";
     words__contents.style.fontSize = "18px";
 }
+function auto__save__value(noidung){
+    let words__contents = document.getElementById("content-words").value;
+    localStorage.setItem('noidung',words__contents);
+}
+check__show__data();
+function check__show__data(){
+    localStorage.setItem('Client_checker', window.location);
+    var value__local = localStorage.getItem('noidung');
+    console.log(value__local);
+    document.getElementById("content-words").innerHTML = value__local;
+}
