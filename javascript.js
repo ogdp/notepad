48580abc3a0090f1e9a8016c66718f1a4ce4d597
MinhDuc <ogdp.vn@gmail.com>
2022-10-26 21:46:46 +0700

    update ip, time update text, bg theme

--- javascript.js
+++ javascript.js
@@ -1,36 +1,55 @@
-
-function font__family__select(font){
-    var font__223 = document.getElementById("slect__font").value;
-    let words__contents = document.getElementById("content-words");
-    words__contents.style.fontFamily = font__223;
+function font__family__select(font) {
+  var font__223 = document.getElementById("slect__font").value;
+  let words__contents = document.getElementById("content-words");
+  words__contents.style.fontFamily = font__223;
 }
-function size__font__select(size){
-    var size__2233 = document.getElementById("change__size__font").value;
-    let words__contents = document.getElementById("content-words");
-    words__contents.style.fontSize = size__2233+"px";
-} 
-function remove__content_words(){
-    let words__contents = document.getElementById("content-words");
-    words__contents.value = "";
+function size__font__select(size) {
+  var size__2233 = document.getElementById("change__size__font").value;
+  let words__contents = document.getElementById("content-words");
+  words__contents.style.fontSize = size__2233 + "px";
 }
-function bold__words(){
-    let words__contents = document.getElementById("content-words");
-    words__contents.style.fontWeight = "bold";
+function remove__content_words() {
+  let words__contents = document.getElementById("content-words");
+  words__contents.value = "";
 }
-function reset__words(){
-    let words__contents = document.getElementById("content-words");
-    words__contents.style.fontWeight = "400";
-    words__contents.style.fontFamily = "Poppins, sans-serif";
-    words__contents.style.fontSize = "18px";
+function bold__words() {
+  let words__contents = document.getElementById("content-words");
+  words__contents.style.fontWeight = "bold";
 }
-function auto__save__value(noidung){
-    let words__contents = document.getElementById("content-words").value;
-    localStorage.setItem('noidung',words__contents);
+function reset__words() {
+  let words__contents = document.getElementById("content-words");
+  words__contents.style.fontWeight = "400";
+  words__contents.style.fontFamily = "Poppins, sans-serif";
+  words__contents.style.fontSize = "18px";
 }
-check__show__data();
-function check__show__data(){
-    localStorage.setItem('Client_checker', window.location);
-    var value__local = localStorage.getItem('noidung');
-    console.log(value__local);
-    document.getElementById("content-words").innerHTML = value__local;
+function auto__save__value(noidung) {
+  let words__contents = document.getElementById("content-words").value;
+  localStorage.setItem("noidung", words__contents);
+  let today = new Date();
+  let date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  let time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  let thoigian = date + " | " + time;
+  localStorage.setItem("Time", thoigian);
+}
+if (localStorage.getItem("noidung") == null) {
+  let today = new Date();
+  let date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  let time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  let thoigian = date + " | " + time;
+  document.querySelector(".time__update").innerHTML = thoigian;
+  localStorage.setItem("Time", thoigian);
+} else {
+  document.querySelector(".time__update").innerHTML =
+    localStorage.getItem("Time");
+  var value__local = localStorage.getItem("noidung");
+  document.getElementById("content-words").innerHTML = value__local;
+}
+get__ip();
+function get__ip(){
+    let client__ip = document.getElementById("ip_client");
+    fetch('https://api.ipify.org/?format=json').then(results=>results.json()).then(data => client__ip.innerHTML=(data.ip));
 }
